89a5cbbbc8356b7a0d37b970fef683b68673e7f3
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-11-29 16:25:04 -0300

    fix: corrigindo erro nas imagens, header e  logo do whatzap

--- script/modal-natal.js
+++ script/modal-natal.js
@@ -3,12 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const modalShown = sessionStorage.getItem("natalModalShown");
     const openModalButton = document.getElementById("openModalButton");
 
-    if (!modalShown) {
-      setTimeout(() => {
-        modal.show();
-        sessionStorage.setItem("natalModalShown", "true");
-      }, 1000)
-    }
 
     openModalButton.addEventListener('click', () => {
       modal.show();
