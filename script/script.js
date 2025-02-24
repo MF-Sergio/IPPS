f56b69fdca8a41795d8d3e8e9c2bee55f55d17c2
Sérgio M. Filho <sergiohrznt@gmail.com>
2025-02-24 17:49:45 -0300

    style: tachinhas postas sobre alguns cartões e cabeçalho fora da foto do banner

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ const mostrarSMPD = function () {
 
 window.onscroll = function () {
     const header = document.getElementById("header");
-    if (window.scrollY > 75) {
+    if (window.scrollY > 100) {
         header.classList.add("header-scrolled");
     } else {
         header.classList.remove("header-scrolled");
