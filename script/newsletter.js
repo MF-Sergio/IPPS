c658b81f1336f64007200d3948073c6d7b5ce88d
Sérgio M. Filho <sergiohrznt@gmail.com>
2026-03-28 19:44:52 -0300

    feat/fix: integração do brevo ao site/ajustes na organização do style.css

---
+++ script/newsletter.js
@@ -0,0 +1,21 @@
+window.REQUIRED_CODE_ERROR_MESSAGE = 'Escolha um código de país';
+window.LOCALE = 'pt';
+window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "A informação fornecida não é valida. Verifique o formato do campo e tente novamente.";
+
+window.REQUIRED_ERROR_MESSAGE = "Este campo não pode ser deixado em branco. ";
+
+window.GENERIC_INVALID_MESSAGE = "A informação fornecida não é valida. Verifique o formato do campo e tente novamente.";
+
+
+
+
+window.translation = {
+    common: {
+        selectedList: '{quantity} lista selecionada',
+        selectedLists: '{quantity} listas selecionadas',
+        selectedOption: '{quantity} selecionado',
+        selectedOptions: '{quantity} selecionados',
+    }
+};
+
+var AUTOHIDE = Boolean(0);
