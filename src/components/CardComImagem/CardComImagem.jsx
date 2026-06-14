9219aa839eefee9a71d4217795f3a7324a3e21b6
Twistywasabi <lucas-hideaki@hotmail.com>
2026-06-13 23:21:59 -0300

    feat: template saiba mais

---
+++ src/components/CardComImagem/CardComImagem.jsx
@@ -0,0 +1,19 @@
+import ImagemPublic from "../ImagemPublic/ImagemPublic";
+
+export function CaixaComImagem(props) {
+  return (
+    <div className="flex flex-col bg-white-100 rounded-lg border-1 border-[var(--borda)]-300 items-center justify-center w-96 content-center">
+      <div className="flex items-center justify-center">
+        <ImagemPublic nomeImagem={props.imagemUrl} />
+      </div>
+      <br />
+      <h2 className="text-xl font-bold mb-4 text-[var(--titulo)]">
+        {props.titulo}
+      </h2>
+      <p className="p-6 text-justify">{props.descricao}</p>
+      <button>Saiba mais</button>
+    </div>
+  );
+}
+
+export default CaixaComImagem;
--- src/pages/Home/components/NossosProjetos/NossosProjetos.jsx
+++ src/pages/Home/components/NossosProjetos/NossosProjetos.jsx
@@ -6,6 +6,7 @@ import {
   PeopleFill,
   ShieldCheck,
 } from "react-bootstrap-icons";
+import CaixaComImagem from "../../../../components/CardComImagem/CardComImagem.jsx";
 
 export function NossosProjetos() {
   return (
@@ -15,6 +16,19 @@ export function NossosProjetos() {
         Conheça algumas das iniciativas que impactam diretamente a vida de
         milhares de pessoas.{" "}
       </p>
+      <br />
+      <div className="flex flex-wrap gap-6 justify-center">
+        <CaixaComImagem
+          imagemUrl="img_vivendo_acolhendo.jpg"
+          titulo="Vivendo e Acolhendo"
+          descricao="Serviço de Proteção Social Especial voltado a Pessoas com Deficiência e suas famílias, promovendo inclusão social e qualidade de vida por meio de atendimento integral, atividades coletivas e suporte humanizado."
+        ></CaixaComImagem>
+        <CaixaComImagem
+          imagemUrl="img_saude_integral.jpg"
+          titulo="Saúde Integral"
+          descricao="Espaço que integra ensino, pesquisa e atendimento à comunidade, promovendo saúde, autonomia e qualidade de vida  através de ações desenvolvidas em parceria com a Clínica Escola Castelo Branco."
+        ></CaixaComImagem>
+      </div>
     </BlocoLayout>
   );
 }
