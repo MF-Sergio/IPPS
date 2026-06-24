86147db4171e0686b4dc026f67a629a04cb6b80c
Sérgio M. Filho <sergiohrznt@gmail.com>
2026-06-23 23:24:51 -0300

    fix: colocada a logo antiga(verde) no footer

--- src/components/Footer/FooterLogo.tsx
+++ src/components/Footer/FooterLogo.tsx
@@ -3,7 +3,7 @@ import Logo from '../Logo/Logo';
 export default function FooterLogo() {
   return (
     <div className="max-w-sm">
-      <Logo imageClassName="h-24 w-auto" />
+      <Logo variant="default" imageClassName="h-24 w-auto" />
 
       <p className="mt-5 text-sm leading-6 text-white/85">
         Instituto de Pesquisa e Promoção da Saúde. Desde 2002 promovendo inclusão, cidadania e autonomia para pessoas com deficiência.
