ebb7860065f315f8531d32c51a5357ba13578833
Sérgio M. Filho <sergiohrznt@gmail.com>
2026-06-25 18:08:04 -0300

    build:  adicionada a rota para a página quem-somos

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,7 +1,9 @@
 import Home from "../pages/Home";
 import SejaParceiro from "../pages/SejaParceiro";
+import QuemSomos from "../pages/QuemSomos";
 
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "/seja-parceiro", element: <SejaParceiro /> },
+  { path: "/quem-somos", element: <QuemSomos /> },
 ];
