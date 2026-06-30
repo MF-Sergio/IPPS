44ef2401a68c170a5ed1d825cdc03568c4d372f4
Sérgio M. Filho <sergiohrznt@gmail.com>
2026-06-30 14:12:05 -0300

    fix: teste de ajuste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react';
-import { Botao } from '../Botao/Botao.tsx';
-import Logo from '../Logo/Logo';
-import HeaderMobileToggle from './HeaderMobileToggle';
-import HeaderNav from './HeaderNav';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { useState } from "react";
+import { Botao } from "../Botao/Botao.tsx";
+import Logo from "../Logo/Logo";
+import HeaderMobileToggle from "./HeaderMobileToggle";
+import HeaderNav from "./HeaderNav";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const navLinks = [
-  { label: 'Quem somos', href: '/quem-somos' },
-  { label: 'Impacto', href: '/impacto' },
-  { label: 'Projetos', href: '/projetos' },
-  { label: 'Abrangência', href: '/abrangencia' },
-  { label: 'Seja Parceiro', href: '/seja-parceiro' },
-  { label: 'Transparência', href: '/transparencia' },
-  { label: 'Eventos', href: '/eventos' },
+  { label: "Quem somos", href: "/quem-somos" },
+  { label: "Impacto", href: "/impacto" },
+  { label: "Projetos", href: "/projetos" },
+  { label: "Abrangência", href: "/abrangencia" },
+  { label: "Seja Parceiro", href: "/seja-parceiro" },
+  { label: "Transparência", href: "/transparencia" },
+  { label: "Eventos", href: "/eventos" },
 ];
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="mt-10 mx-auto flex w-full max-w-[1150px] px-4 sm:px-6 lg:px-8 text-black items-center justify-between my-6 font-semibold relative z-50">
+    <header className="mt-9 mx-auto flex w-full max-w-[1150px] px-4 sm:px-6 lg:px-8 text-black items-center justify-between my-6 font-semibold relative z-50">
       <Logo variant="nova" />
 
-      <HeaderMobileToggle
-        isOpen={isOpen}
-        onClick={() => setIsOpen(!isOpen)}
-      />
+      <HeaderMobileToggle isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
 
       <div
-        className={`absolute md:static top-16 left-0 right-0 md:flex md:items-center gap-9 bg-white md:bg-transparent z-50 md:z-auto ${isOpen ? 'flex flex-col' : 'hidden md:flex'}`}
-        >
+        className={`absolute md:static top-16 left-0 right-0 md:flex md:items-center gap-9 bg-white md:bg-transparent z-50 md:z-auto ${isOpen ? "flex flex-col" : "hidden md:flex"}`}
+      >
         <HeaderNav navLinks={navLinks} onNavigate={() => setIsOpen(false)} />
         <Botao
           texto="Doar Agora"
